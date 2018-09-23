95d73832b3de57ad6ef28462a872a25a062c4887
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-23 19:57:57 +0300

    Update ml5om5.js

--- ml5om5.js
+++ ml5om5.js
@@ -207,7 +207,7 @@ function play(guild, song) {
 }
 
 const adminprefix = "$vip";
-const devs = ['274923685985386496'];
+const devs = ['488334414124810240'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
